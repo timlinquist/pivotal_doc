390a613d6633f60d2b925f6a10770d7f6c97f9a3
Tim Linquist <timo3377@gmail.com>
2010-09-17 16:49:47 -0700

    Laid out next steps for gen

--- spec/lib/generator_spec.rb
+++ spec/lib/generator_spec.rb
@@ -2,5 +2,30 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Generator do
   before(:each) do
+    pending
+  end
+  
+  it "should initialize the configuration with the default connection" do
+    
+  end
+  
+  describe "Projects" do
+    it "should get the latest iteration for the project" do
+      
+    end
+        
+    it "should group the stories for the latest iteration by type" do
+      
+    end
+  end
+  
+  describe "Formattting" do
+    it "should use the formatter specified in the configuration" do
+      
+    end
+    
+    it "should render the groups of stories through the formatter" do
+      
+    end
   end
 end
