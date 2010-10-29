c2420588febae00db0a9ff4e338e39f3d9fd6ca3
Tim Linquist <timo3377@gmail.com>
2010-10-29 11:10:30 -0700

    Fixed loading of child generators

--- lib/pivotal_doc.rb
+++ lib/pivotal_doc.rb
@@ -5,5 +5,11 @@ require 'haml'
 PT= PivotalTracker unless defined?(PT)
 PROJECT_ROOT= File.join(File.dirname(__FILE__), '/../') unless defined?(PROJECT_ROOT)
 
+#Load generators explicitly
+path= File.dirname(__FILE__) + '/pivotal_doc/generators/'
+require File.join(path, 'base.rb')
+require File.join(path,'html.rb')
+require File.join(path,'text.rb')
+
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'../ext','**','*.rb'))].each {|f| require f}
-Dir[File.expand_path(File.join(File.dirname(__FILE__),'pivotal_doc/','**','*.rb'))].each {|f| require f}
+Dir[File.expand_path(File.join(File.dirname(__FILE__),'pivotal_doc/', '*.rb'))].each {|f| require f}
